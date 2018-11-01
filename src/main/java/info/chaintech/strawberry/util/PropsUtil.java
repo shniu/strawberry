07ed45e57ca81eb209acedf124e6e10db96f88a6
shniu <niushaohan@digcredit.com>
2018-11-01 21:04:14 +0800

    add unit test

--- src/main/java/info/chaintech/strawberry/util/PropsUtil.java
+++ src/main/java/info/chaintech/strawberry/util/PropsUtil.java
@@ -3,6 +3,7 @@ package info.chaintech.strawberry.util;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.InputStream;
 import java.util.Properties;
 
 /**
@@ -12,7 +13,23 @@ public class PropsUtil {
     private static final Logger logger = LoggerFactory.getLogger(PropsUtil.class);
 
 
-    public static Properties loadProps(String configProps) {
-        return null;
+    /**
+     * 加载配置文件
+     *
+     * @param propsPath 文件路径
+     * @return Properties
+     */
+    public static Properties loadProps(String propsPath) {
+        Properties props = new Properties();
+
+        InputStream inputStream;
+
+        if (StringUtil.isEmpty(propsPath)) {
+            throw new IllegalArgumentException();
+        }
+
+        String suffix = ".properties";
+
+        return props;
     }
 }
---
+++ src/main/test/java/info/chaintech/strawberry/core/ClassHelperTest.java
@@ -0,0 +1,36 @@
+package info.chaintech.strawberry.core;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created by Administrator on 2018/11/1 0001.
+ */
+public class ClassHelperTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void getClassList() throws Exception {
+
+    }
+
+    @Test
+    public void getClassListBySuper() throws Exception {
+
+    }
+
+    @Test
+    public void getClassListByAnnotation() throws Exception {
+
+    }
+
+}
