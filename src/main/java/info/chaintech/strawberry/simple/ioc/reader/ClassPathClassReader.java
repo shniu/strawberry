ae47b1ac10c7c566927c8ad58ebc68a0f29011b7
shniu <niushaohan@digcredit.com>
2018-11-14 10:31:44 +0800

    tony

---
+++ src/main/java/info/chaintech/strawberry/simple/ioc/reader/ClassPathClassReader.java
@@ -0,0 +1,20 @@
+package info.chaintech.strawberry.simple.ioc.reader;
+
+import info.chaintech.strawberry.simple.ioc.ClassReader;
+import info.chaintech.strawberry.simple.ioc.bean.ClassInfo;
+import info.chaintech.strawberry.simple.ioc.bean.Scanner;
+
+import java.util.Set;
+
+/**
+ * Classpath class reader
+ * <p>
+ * Created by shniu on 2018/11/13.
+ */
+public class ClassPathClassReader implements ClassReader {
+
+    @Override
+    public Set<ClassInfo> readClasses(Scanner scanner) {
+        return null;
+    }
+}
---
+++ src/main/java/info/chaintech/strawberry/simple/ioc/reader/JarClassReader.java
@@ -0,0 +1,19 @@
+package info.chaintech.strawberry.simple.ioc.reader;
+
+import info.chaintech.strawberry.simple.ioc.ClassReader;
+import info.chaintech.strawberry.simple.ioc.bean.ClassInfo;
+import info.chaintech.strawberry.simple.ioc.bean.Scanner;
+
+import java.util.Set;
+
+/**
+ * Jar class reader
+ * <p>
+ * Created by shniu on 2018/11/13.
+ */
+public class JarClassReader implements ClassReader {
+    @Override
+    public Set<ClassInfo> readClasses(Scanner scanner) {
+        return null;
+    }
+}
